864c649fbc9108f2258395d5f3090d64a74d33d4
moa1128 <moa971128@gmail.com>
2024-09-25 23:28:18 -0700

    feat: add Navbar component & set metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,22 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/styles/theme';
+import RootLayout from '@/components/layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+      <Head>
+        <title>Music Feed</title>
+        <meta name="description" content="Have fun" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
     </ThemeProvider>
   );
 }
