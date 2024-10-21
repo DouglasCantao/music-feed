5dd3def7a6c372f443abbaae67af2adc19790d22
moa1128 <moa971128@gmail.com>
2024-10-21 00:29:52 -0700

    fix: modify req.query to req.body

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,24 +1,28 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const axios = require('axios');
+const axios = require("axios");
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
-async function getSearch(term, limit=5, offset=0) {
-
+async function getSearch(term, limit = 5, offset = 0) {
   const encodeString = (str) => {
-    return encodeURIComponent(str).replace(/%20/g, '+');
-  }
+    return encodeURIComponent(str).replace(/%20/g, "+");
+  };
 
   try {
     // First, get an access token
-    const tokenResponse = await axios.post('https://accounts.spotify.com/api/token', 
-      'grant_type=client_credentials', {
-      headers: {
-        'Authorization': 'Basic ' + (Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')),
-        'Content-Type': 'application/x-www-form-urlencoded'
+    const tokenResponse = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      "grant_type=client_credentials",
+      {
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
-    });
+    );
 
     const accessToken = tokenResponse.data.access_token;
 
@@ -26,46 +30,43 @@ async function getSearch(term, limit=5, offset=0) {
     const response = await axios.get(`https://api.spotify.com/v1/search`, {
       params: {
         q: `${encodeString(term)}`,
-        type: 'track',
+        type: "track",
         limit: limit,
-        offset: offset
+        offset: offset,
       },
       headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
 
     // Extract and return the tracks
     const resp = response.data.tracks.items.map((song) => {
- 
       return {
         id: song.id,
         name: song.name,
         artist: song.artists[0].name,
         album: song.album.name,
-        cover: song.album.images[0].url
+        cover: song.album.images[0].url,
       };
-
     });
 
     return resp;
   } catch (error) {
-    console.error('Error fetching artists:', error);
+    console.error("Error fetching artists:", error);
     return [];
   }
-
 }
 
 type ResponseData = {
-  tracks: Array<string> | undefined,
-}
+  tracks: Array<string> | undefined;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+  const { term, limit, offset } = req.body;
 
-  const { term, limit, offset } = req.query;
   const tracks = await getSearch(term, limit, offset);
   res.status(200).json({ tracks });
 }
